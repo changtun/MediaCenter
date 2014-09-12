86d2f656afe964bb2f1aa83b85d143a8bf3fffb3
sguotao <sguotao@gmail.com>
2014-09-12 14:24:01 +0800

    2014-09-12 add lrc bean

---
+++ https:/github.com/sguotao/MediaCenter.git/MediaCenter/src/com/pbi/center/utils/LRCUtil.java
@@ -0,0 +1,23 @@
+/*
+ * File Name:  LRCUtil.java
+ * Copyright:  Beijing Jaeger Communication Electronic Technology Co., Ltd. Copyright YYYY-YYYY,  All rights reserved
+ * Descriptions:  <Descriptions>
+ * Changed By:  
+ * Changed Time:  2014-9-12
+ * Changed Content:  <Changed Content>
+ */
+package com.pbi.center.utils;
+
+/**
+ * <Functional overview>
+ * <Functional Details>
+ * 
+ * @author  
+ * @version  [Version Number, 2014-9-12]
+ * @see  [Relevant Class/Method]
+ * @since  [Product/Module Version]
+ */
+public class LRCUtil
+{
+    
+}
